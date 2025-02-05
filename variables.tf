ff01c6e5d765f427ff12648b4c972b452953638a
jayanthi gunja <jayanthi.tech94@gmail.com>
2025-02-05 19:00:46 +0530

    VPC module

--- variables.tf
+++ variables.tf
@@ -23,8 +23,78 @@ variable "common_tags" {
 variable "vpc_tags" {
   type        = map
   default     = {
-  }
-  
+  }  
 }
+
+variable "igw_tags" {
+  type        = map
+  default     = {}
+}
+
+
+variable "public_subnet_cidrs" {
+  type        = list
+  validation {
+    condition     = length (var.public_subnet_cidrs) == 2
+    error_message = "Please provide two valid public cidrs"
+  }
+}
+
+variable "public_subnet_tags" {
+default = {}
+}
+
+variable "private_subnet_cidrs" {
+  type        = list
+  validation {
+    condition     = length (var.private_subnet_cidrs) == 2
+    error_message = "Please provide two valid private cidrs"
+  }
+}
+
+variable "private_subnet_tags" {
+default = {}
+}
+
+variable "database_subnet_cidrs" {
+  type        = list
+  validation {
+    condition     = length (var.database_subnet_cidrs) == 2
+    error_message = "Please provide two valid database cidrs"
+  }
+}
+
+variable "database_subnet_tags" {
+default = {}
+}
+
+variable "nat_gateway_tags" {
+  default     = {}
+}
+
+variable "public_route_table_tags" {
+  default     = {} 
+}
+
+variable "private_route_table_tags" {
+  default     = {} 
+}
+
+variable "database_route_table_tags" {
+  default     = {} 
+}
+
+variable "is_peering_required" {
+  default     = false
+}
+
+variable "vpc_peering_tags" {  
+  default     = {}
+}
+
+
+
+
+
 
 
